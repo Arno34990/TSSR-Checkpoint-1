133151b9649c6e751f4e630a86e0df943886f4de
Arno <61407627+Arno34990@users.noreply.github.com>
2024-10-25 12:22:51 +0200

    Update addUsers.sh

--- Script_de_création_d_utilisateurs_en_bash/addUsers.sh
+++ Script_de_création_d_utilisateurs_en_bash/addUsers.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 # Verifier les arguments:
-if [ $# -eq 0 ]
+if [ $# == 0 ]
     then
         echo "Il manque les noms d'utilisateurs en argument - Fin du script"
         exit 1
@@ -11,10 +11,18 @@ fi
 for nom in $@
 do
     # Verifier si l'id de l'utilisateur existe déjà sinon il le crée
-    if id $nom &>/dev/null
+    if [ id $nom &>/dev/null ]
         then
             echo "L'utilisateur $nom existe déjà"
         else
             sudo useradd $nom
+            # Vérification si l'utilisateur est crée sinon message erreur
+            if [ $? == 0 ]
+                then
+                    echo "L'utilisateur $nom a été crée"
+                else
+                    echo "Erreur à la création de l'utilisateur $nom"
+            fi
+    fi
 done
 exit 0
